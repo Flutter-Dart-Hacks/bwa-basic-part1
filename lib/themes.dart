906a1c2a8111127e23909be8384506601e546987
GulajavaMinistudio <gulajava.mini@gmail.com>
2022-09-19 16:37:15 +0700

    Init project awal

---
+++ lib/themes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color colorPrimaryText = Color(0xff314728);
+const Color colorGrey = Color(0xffA9B0A6);
+const Color colorOrange = Color(0xffF9B650);
+const Color colorGreyBg = Color(0xffE5E5E5);
