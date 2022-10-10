5b92463d580655f82f31935ce3a30946918d96a8
GulajavaMinistudio <gulajava.mini@gmail.com>
2022-10-10 16:21:44 +0700

    Membuat dialog bottom sheet

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,30 +9,270 @@ class HomePageMain extends StatefulWidget {
 }
 
 class _HomePageMainState extends State<HomePageMain> {
+  generateRowWidgetdata() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.asset(
+              'resources/images/item1.png',
+              width: 80,
+              height: 80,
+              fit: BoxFit.contain,
+            ),
+            Image.asset(
+              'resources/images/item2.png',
+              width: 80,
+              height: 80,
+              fit: BoxFit.contain,
+            ),
+            Image.asset(
+              'resources/images/item3.png',
+              width: 80,
+              height: 80,
+              fit: BoxFit.contain,
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 40,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.asset(
+              'resources/images/item4.png',
+              width: 80,
+              height: 80,
+              fit: BoxFit.contain,
+            ),
+            Image.asset(
+              'resources/images/item5.png',
+              width: 80,
+              height: 80,
+              fit: BoxFit.contain,
+            ),
+            Image.asset(
+              'resources/images/item6.png',
+              width: 80,
+              height: 80,
+              fit: BoxFit.contain,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  showDialogBelakang(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 200,
+          color: Colors.white,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+          ),
+          child: Column(
+            children: const [],
+          ),
+        );
+      },
+      isDismissible: true,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: colorBgAbu,
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              children: const [
-                SizedBox(
-                  height: 20,
+          child: Scrollbar(
+            trackVisibility: true,
+            child: SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Profile Picture',
+                      style: TextStyle(
+                        color: colorPrimaryText,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Image.asset(
+                      'resources/images/primary.png',
+                      width: 140,
+                      height: 140,
+                      fit: BoxFit.contain,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    const Text(
+                      'Kucing Coklat Oren',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: colorPrimaryText,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    const Text(
+                      'UI/UX Designer',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        color: colorGrey,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 70,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset(
+                          'resources/images/item1.png',
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.contain,
+                        ),
+                        Image.asset(
+                          'resources/images/item2.png',
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.contain,
+                        ),
+                        Image.asset(
+                          'resources/images/item3.png',
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.contain,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Image.asset(
+                          'resources/images/item4.png',
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.contain,
+                        ),
+                        Image.asset(
+                          'resources/images/item5.png',
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.contain,
+                        ),
+                        Image.asset(
+                          'resources/images/item6.png',
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.contain,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Wrap(
+                      spacing: 38,
+                      runSpacing: 20,
+                      children: [
+                        Image.asset(
+                          'resources/images/item1.png',
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.contain,
+                        ),
+                        Image.asset(
+                          'resources/images/item2.png',
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.contain,
+                        ),
+                        Image.asset(
+                          'resources/images/item3.png',
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.contain,
+                        ),
+                        Image.asset(
+                          'resources/images/item4.png',
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.contain,
+                        ),
+                        Image.asset(
+                          'resources/images/item5.png',
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.contain,
+                        ),
+                        Image.asset(
+                          'resources/images/item6.png',
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.contain,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    SizedBox(
+                      width: 200,
+                      height: 50,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          showDialogBelakang(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green.shade300,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(30),
+                            ),
+                          ),
+                        ),
+                        child: const Text(
+                          'Update Profile',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                  ],
                 ),
-                Text(
-                  'Profile Picture',
-                  style: TextStyle(
-                    color: colorPrimaryText,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-              ],
+              ),
             ),
           ),
         ),
--- lib/themes.dart
+++ lib/themes.dart
@@ -4,3 +4,4 @@ const Color colorPrimaryText = Color(0xff314728);
 const Color colorGrey = Color(0xffA9B0A6);
 const Color colorOrange = Color(0xffF9B650);
 const Color colorGreyBg = Color(0xffE5E5E5);
+const Color colorBgAbu = Color(0xfff2f2f2);
